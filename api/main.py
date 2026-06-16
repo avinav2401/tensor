77fcb3dce661e11c755f0c17794a176e2e45451e
Avinav priyadarshi <166238765+avinav2401@users.noreply.github.com>
2026-06-16 21:43:58 +0530

    feat: read PORT from environment variables in main.py

--- api/main.py
+++ api/main.py
@@ -51,5 +51,8 @@ async def predict(
         'confidence': float(confidence)
     }
 
+import os
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
